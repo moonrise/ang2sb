4d6a0f42e07ef1eaec8a480eecdaed4869abc2ef
bjeong <bjjeong@cisco.com>
2016-01-05 14:47:45 -0800

    done with hero edtior

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,8 +1,28 @@
 import {Component} from 'angular2/core'
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>ng2 app</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <!-- <input value="{{hero.name}}" placeholder="name"> -->
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
 })
 
-export class AppComponent {}
+export class AppComponent {
+  public title: string = 'Tour of Heroes';
+  public hero: Hero = {
+    id: 2,
+    name: 'Windstorm'
+  };
+}
